46dcc5d32fb2f59e8caf312c7ae1604236d9043c
prathima <rameshchigullapally977@gmail.com>
2019-06-13 00:57:39 -0700

    prathima

---
+++ Assets/OurBattleRoyale/scripts/pooling system/cubespawner.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cubespawner : MonoBehaviour
+{
+    ObjectPooler objectPooler;
+    private void Start()
+    {
+        objectPooler = ObjectPooler.Instance;
+    }
+    void FixedUpdate()
+    {
+        objectPooler.SpawnFromPool("cube", transform.position, Quaternion.identity);
+    }
+}

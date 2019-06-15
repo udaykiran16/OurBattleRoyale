823085c64e2bee42cc7805cd4b00146e942a3cae
uday <udaybojja16@gmail.com>
2019-06-15 16:03:48 +0530

    New Network Manager Part1

---
+++ Assets/OurBattleRoyale/scripts/Networking/MultiplayerSettings.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiplayerSettings : MonoBehaviour
+{
+    public static MultiplayerSettings multiplayerSettings;
+
+    public bool delayStart;
+    public int maxPlayers;
+
+    public int menuScene;
+    public int multiplayerScene;
+
+    private void Awake()
+    {
+        if (MultiplayerSettings.multiplayerSettings == null)
+        {
+            MultiplayerSettings.multiplayerSettings = this;
+        }
+        else
+        {
+            if (MultiplayerSettings.multiplayerSettings != null)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+
+}

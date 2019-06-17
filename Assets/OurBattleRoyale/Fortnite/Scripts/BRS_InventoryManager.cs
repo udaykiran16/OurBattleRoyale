8f15a48e48e4162e914326d1a5171772c295bd93
uday <udaybojja16@gmail.com>
2019-06-17 11:49:00 +0530

    animated pickups Added

---
+++ Assets/OurBattleRoyale/Fortnite/Scripts/BRS_InventoryManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//=============================================================================
+// This script was created by Matt Parkin as a part of the Polygon Pilgrimage
+// Go to www.youtube.com/mattparkin and subscribe for more awesome tutorials!
+// You are free to use/modify/edit this script however you like.  Please give
+// credit to the orginal creator and share your work!
+//=============================================================================
+public class BRS_InventoryManager : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	public void AddToInventory()
+	{
+		Debug.Log ("added to inventory");
+	}
+}
